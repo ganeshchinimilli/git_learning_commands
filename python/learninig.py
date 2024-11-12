fff1f4762a30f1d7cbff938c3e3855a4f92a1f43
ganeshchinimilli <ganeshchinimilli4@gmail.com>
2024-11-12 21:48:22 +0530

    11/12/2024

---
+++ python/learninig.py
@@ -0,0 +1,42 @@
+# import sys
+# print(sys.version)
+"""
+if(5>2):
+    print('testing')
+    """
+
+# casting of variables
+# x=5
+# y=10
+# x1=int(5)->5
+# x2=str(5)->'5'
+
+# to get the type of the function 
+# print(type(x))
+
+# case sensitive
+# x=5
+# X='tesitng'
+# print(x,X)
+
+# camel_case  myVaraiblName
+# pascal_case MyVariableName
+# snake_case my_variable_name
+
+# single line values assigning are 
+# x,y,z = 1,2,3
+# unpacking a collection will be 
+# x,y,z =['testi','tee','teet']
+
+# global keyword will delcare any variable as global
+
+# datatypes in python are
+
+# str --text type
+# int,float,complex(ij) -- Numeric
+# list(['1',2]),tuple((1,2)),range(range(9))--sequence types
+# dict{'':''} --mapping
+# set({'as','asd'}),frozenset(frozenset({'sad','asd'})) -- set
+# bool--boolean
+# bytes(b"hel"),bytearray(bytearray(5)),memoryview(bytes(4))--binarytypes
+
